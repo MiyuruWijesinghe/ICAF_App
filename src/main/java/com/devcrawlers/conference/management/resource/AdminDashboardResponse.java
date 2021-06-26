f51611effb58384d6a98ed6b1cb866a41fc4f082
MiyuruWijesinghe <mkwijesinghe2016@gmail.com>
2021-06-26 16:45:34 +0530

    updated admin dashboard

--- src/main/java/com/devcrawlers/conference/management/resource/AdminDashboardResponse.java
+++ src/main/java/com/devcrawlers/conference/management/resource/AdminDashboardResponse.java
@@ -5,4 +5,34 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
 public class AdminDashboardResponse {
 
+	private String totalRoles;
+	
+	private String totalEditors;
+	
+	private String totalReviewers;
+
+	public String getTotalRoles() {
+		return totalRoles;
+	}
+
+	public void setTotalRoles(String totalRoles) {
+		this.totalRoles = totalRoles;
+	}
+
+	public String getTotalEditors() {
+		return totalEditors;
+	}
+
+	public void setTotalEditors(String totalEditors) {
+		this.totalEditors = totalEditors;
+	}
+
+	public String getTotalReviewers() {
+		return totalReviewers;
+	}
+
+	public void setTotalReviewers(String totalReviewers) {
+		this.totalReviewers = totalReviewers;
+	}
+	
 }
--- src/main/java/com/devcrawlers/conference/management/service/impl/DashboardServiceImpl.java
+++ src/main/java/com/devcrawlers/conference/management/service/impl/DashboardServiceImpl.java
@@ -1,8 +1,11 @@
 package com.devcrawlers.conference.management.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devcrawlers.conference.management.repository.RolesRepository;
 import com.devcrawlers.conference.management.resource.AdminDashboardResponse;
 import com.devcrawlers.conference.management.resource.ReviewerDashboardResponse;
 import com.devcrawlers.conference.management.service.DashboardService;
@@ -11,10 +14,24 @@ import com.devcrawlers.conference.management.service.DashboardService;
 @Transactional(rollbackFor=Exception.class)
 public class DashboardServiceImpl implements DashboardService {
 
+	@Autowired
+	private Environment environment;
+
+	@Autowired
+	private RolesRepository rolesRepository;
+	
+	
+	
 	@Override
 	public AdminDashboardResponse getAdminDashboardDetails() {
 		
-		return null;
+		AdminDashboardResponse adminDashboardResponse = new AdminDashboardResponse();
+		
+		Long totalRoles = rolesRepository.count();
+		
+		adminDashboardResponse.setTotalRoles(totalRoles.toString());
+		
+		return adminDashboardResponse;
 	}
 
 	@Override
